e92aa7c237545213e3a483e307b512f29a851ca9
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-07-01 11:07:48 -0400

    still building logic for master giant

--- Scripts/master_giant.gd
+++ Scripts/master_giant.gd
@@ -1,1 +1,23 @@
 extends Node2D
+
+@onready var animation_tree = $AnimationTree
+
+const atk_threshold = 1
+const swipes = ["left_swipe", "right_swipe"]
+
+var idle_count = 0
+var atk_set = swipes
+
+func _ready():
+	randomize()
+
+func increase_idle_count():
+	idle_count += 1
+	
+	if idle_count > atk_threshold:
+		idle_count = 0
+		attack()
+
+func attack():
+	var atk = atk_set
+	
