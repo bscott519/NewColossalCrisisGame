1298289b9294958ee98ca105e532477ff858d553
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-07-06 16:30:05 -0400

    added flagpole to ending screen at end of level 7

---
+++ Scripts/finalflagpole.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+@export var next_scene : String
+var scene_transitioning: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_exit_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		scene_transitioning = true
+		
+		var player = body
+		player.queue_free()
+		
+		get_tree().change_scene_to_file("res://Scenes/endingscreen.tscn")
+		#await get_tree().create_timer(0.5).timeout
+		#Scenemanager.transition_to_scene(next_scene)
