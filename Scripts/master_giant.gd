8d52271f97d54f0ab27b758a4d99917cb395ba61
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-07-10 21:47:49 -0400

    gonna rework final boss

--- Scripts/master_giant.gd
+++ Scripts/master_giant.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree["parameters/playback"]
 
-const atk_threshold = 1
-const swipes = ["left_swipe", "right_swipe"]
+const atk_threshold = 2
+const swipes = ["left_swipe", "right_swipe", "laser_eyes", "left_blast", "right_blast"]
 
 var idle_count = 0
 var atk_set = swipes
 
 func _ready():
 	randomize()
+	animation_tree.active = true
+
+func _on_animation_player_animation_finished(idle):
+	if idle == "idle":
+		increase_idle_count()
 
 func increase_idle_count():
 	idle_count += 1
@@ -19,5 +25,8 @@ func increase_idle_count():
 		attack()
 
 func attack():
-	var atk = atk_set[randi()%atk_set.size()]
-	animation_tree.set_condition(atk, true)
+	var atk = atk_set[randi() % atk_set.size()]
+	
+	for attack in atk_set:
+		animation_tree.set("parameters/conditions/%s" % attack, false)
+	animation_tree.set("parameters/conditions/%s" % atk, true)
