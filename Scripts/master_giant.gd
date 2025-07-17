67743b66c17b7d14e26f1145eaa6a2100cbe7981
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-07-17 12:02:41 -0400

    laser one works now, laser two not working

--- Scripts/master_giant.gd
+++ Scripts/master_giant.gd
@@ -22,6 +22,8 @@ var is_knocked_back: bool = false
 var knockback_dur: float = 0.2
 var is_chasing: bool = false
 var can_walk: bool
+var beam_1_can_dmg = false
+var beam_2_can_dmg = false
 
 var mG_dmg: int = 0
 var can_dmg: bool = true
@@ -145,11 +147,14 @@ func change_state(new_state):
 			print("Entering LASEREYES")
 			velocity = Vector2.ZERO
 			animation_player.play("LaserEyes")
+			#mg_fire_beam_2()
 			attack_cooldown.start()
 
 func _on_animation_player_animation_finished(anim_name):
 	if dead:
 		return
+	#if anim_name == "LaserEyes":
+		#mg_stop_beam_2()
 	if current_state in [State.LEFTSWIPE, State.RIGHTSWIPE, State.LEFTBLAST, State.RIGHTBLAST, State.LASEREYES]:
 			print("Finished attack animation:", current_state)
 			change_state(State.CHASE)
@@ -254,27 +259,31 @@ func _on_head_hit_box_body_entered(body):
 		start_dmg_cooldown()
 
 func mg_fire_beam_1():
+	beam_1_can_dmg = true
 	$MGSprites/MasterBeam/BeamHurtBox.monitoring = true
 	$MGSprites/MasterBeam/BeamHurtBox/BeamCollisionShape.disabled = false
 	$MGSprites/MasterBeam/BeamHurtBox.visible = true
 
 func mg_stop_beam_1():
+	var beam_1_can_dmg = false
 	$MGSprites/MasterBeam/BeamHurtBox.monitoring = false
 	$MGSprites/MasterBeam/BeamHurtBox/BeamCollisionShape.disabled = true
 	$MGSprites/MasterBeam/BeamHurtBox.visible = false
 
 func mg_fire_beam_2():
+	var beam_2_can_dmg = true
 	$MGSprites/MasterBeam2/BeamHurtBox2.monitoring = true
 	$MGSprites/MasterBeam2/BeamHurtBox2/BeamCollisionShape2.disabled = false
 	$MGSprites/MasterBeam2/BeamHurtBox2.visible = true
 
 func mg_stop_beam_2():
+	var beam_2_can_dmg = false
 	$MGSprites/MasterBeam2/BeamHurtBox2.monitoring = false
 	$MGSprites/MasterBeam2/BeamHurtBox2/BeamCollisionShape2.disabled = true
 	$MGSprites/MasterBeam2/BeamHurtBox2.visible = false
 
 func _on_beam_hurt_box_body_entered(body):
-	if can_dmg and body.is_in_group("player"):
+	if can_dmg and beam_1_can_dmg and body.is_in_group("player"):
 		print("Player entered Master Giant laser 1.")
 		var knockback_dir = (body.global_position - global_position).normalized()
 		body.plyr_take_dmg(dmg_to_deal, knockback_dir)
@@ -282,7 +291,8 @@ func _on_beam_hurt_box_body_entered(body):
 		start_dmg_cooldown()
 
 func _on_beam_hurt_box_2_body_entered(body):
-	if can_dmg and body.is_in_group("player"):
+	print("Beam 2 body entered:", body)
+	if can_dmg and beam_2_can_dmg and body.is_in_group("player"):
 		print("Player entered Master Giant laser 2.")
 		var knockback_dir = (body.global_position - global_position).normalized()
 		body.plyr_take_dmg(dmg_to_deal, knockback_dir)
